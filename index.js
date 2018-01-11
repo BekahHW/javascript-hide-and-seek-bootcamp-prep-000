69dff2faffa8e8a4d8b547b884f19956deeed0b2
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-11 17:04:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+
+}
+
+function nestedTarget(){
+return document.querySelector(" #nested .target");
+}
+
+function increaseRankBy(n){
+document.ranked-list(parseInt(n+2))
+}
+
+function deepestChild(){
+  
+}
