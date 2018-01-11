52512c5a33157f1381b87c666a7b880ad7ffcea6
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-11 18:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.ranked-list(parseInt(n+2))
 function deepestChild(){
 var nodes = document.querySelectorAll("#grand-node div");
 var mostDeep = nodes[nodes.length- 1];
+return mostDeep
 
 }
 
