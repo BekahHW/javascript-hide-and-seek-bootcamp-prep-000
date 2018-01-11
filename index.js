850fca6da1f7ec4389b65af33c1a87ad073d5ad6
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-11 18:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ document.ranked-list(parseInt(n+2))
 }
 
 function deepestChild(){
-var nodes = document.querySelectorAll('#grand-node div');
+var nodes = document.querySelectorAll("#grand-node div");
 var mostDeep = nodes[nodes.length- 1];
+
 }
 
